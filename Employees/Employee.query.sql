a5d9bbca9a417662c7a29583fc511f32eb81279d
lumirta <mirta.lucic70@gmail.com>
2023-04-26 19:01:22 -0700

    m

--- Employees/Employee.query.sql
+++ Employees/Employee.query.sql
@@ -1,43 +1,44 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR (20)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
-SELECT * FROM departments
+--SELECT * FROM departments
+
+--drop table dept_emp;
 
 CREATE TABLE "dept_emp" (
-    "emp_no" INTEGER   NOT NULL UNIQUE,
-    "dept_no" VARCHAR   NOT NULL UNIQUE,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "dept_no"
-     )
+    "emp_no" INTEGER  NOT NULL,
+    "dept_no" VARCHAR (4)  NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR   NOT NULL UNIQUE,
-    "emp_no" INTEGER   NOT NULL UNIQUE,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no"
-     )
+    "dept_no" VARCHAR (4)  NOT NULL,
+    "emp_no" INTEGER  NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE "employees" (
     "emp_no" INTEGER   NOT NULL,
-    "emp_title_id" VARCHAR   NOT NULL,
+    "emp_title_id" VARCHAR (5)  NOT NULL,
     "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "sex" VARCHAR   NOT NULL,
+    "first_name" VARCHAR (20)  NOT NULL,
+    "last_name" VARCHAR (20)   NOT NULL,
+    "sex" VARCHAR (1)  NOT NULL,
     "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
-     ),
+     )
 	
 );
 
@@ -50,24 +51,24 @@ CREATE TABLE "salaries" (
 );
 
 CREATE TABLE "titles" (
-    "title_id" VARCHAR   NOT NULL,
-    "title" VARCHAR   NOT NULL,
+    "title_id" VARCHAR (5)  NOT NULL,
+    "title" VARCHAR (20)  NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+--ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+--REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+--ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+--REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+--ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+--REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+--ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+--REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
@@ -116,7 +117,7 @@ SELECT employees.emp_no AS Employee_number, employees.last_name, employees.first
 FROM employees
 JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
 JOIN departments ON departments.dept_no = dept_emp.dept_no
-WHERE departments.dept_name = 'Sales' AND departments.dept_name = 'Development' ;
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development' ;
 
 --8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT employees.last_name, COUNT (last_name) AS Count_last_name
--- Employees/Employee_tables.sql
+++ Employees/Employee_tables.sql
@@ -8,22 +8,24 @@ CREATE TABLE "departments" (
         "dept_no"
      )
 );
-SELECT * FROM departments
+--SELECT * FROM departments
+
+--drop table dept_emp;
 
 CREATE TABLE "dept_emp" (
     "emp_no" INTEGER  NOT NULL,
     "dept_no" VARCHAR (4)  NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "dept_no"
-     )
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR (4)  NOT NULL,
     "emp_no" INTEGER  NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no"
-     )
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE "employees" (
@@ -36,7 +38,7 @@ CREATE TABLE "employees" (
     "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
-     ),
+     )
 	
 );
 
@@ -56,17 +58,17 @@ CREATE TABLE "titles" (
      )
 );
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+--ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+--REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+--ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+--REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+--ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+--REFERENCES "departments" ("dept_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+--ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+--REFERENCES "employees" ("emp_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
